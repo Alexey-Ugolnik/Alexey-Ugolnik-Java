89ee19287a2de8f722fc95162cbe7a18fdced8a6
Alexey-Ugolnik <98695937+Alexey-Ugolnik@users.noreply.github.com>
2022-03-07 16:56:08 +0300

    02

---
+++ 01_Java/02/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива:");
+        int size = scanner.nextInt();
+        double array[] = new double[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("Массив:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + array[i]);
+        }
+        System.out.println();
+        System.out.println("Массив + 10%");
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + array[i] * 1.1);
+        }
+    }
+}
