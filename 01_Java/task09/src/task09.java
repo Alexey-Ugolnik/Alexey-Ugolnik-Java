4e9b385a5e7d44ab74d5c8efb5137b4f65a81cd6
Alexey-Ugolnik <98695937+Alexey-Ugolnik@users.noreply.github.com>
2022-03-19 21:03:15 +0300

    09

---
+++ 01_Java/task09/src/task09.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class task09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int number1 = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int number2 = scanner.nextInt();
+
+        System.out.println("Вы ввели числа: " + number1 + " и " + number2);
+
+        int amount = number1 + number2;
+        number1 = amount - number1;
+        number2 = amount - number2;
+
+        System.out.println("А я поменял их местами: " + number1 + " и " + number2);
+    }
+}
